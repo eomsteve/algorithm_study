70d0ab1667cf823b0b4e1993946bc406bd1e3eaf
su <whrudtn9727@naver.com>
2022-02-15 00:02:17 +0900

    0215 ks

---
+++ 1st_week/jks/no.2386(도비).py
@@ -0,0 +1,11 @@
+eng = []
+while eng != ['#']:
+    eng = list(input().lower().split())
+    k = eng[0]
+    eng_sum = 0
+    for i in range(1, len(eng)):
+        for j in range(len(eng[i])):
+            if k == eng[i][j]:
+                eng_sum += 1
+    if k != '#':
+        print(k, eng_sum)
